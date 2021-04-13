b86a6c956df672cd64790b81eb220700f74f4d50
phamthomas12 <phamthomas12@gmail.com>
2021-04-13 21:21:02 +1000

    created table and pushed user info into it

---
+++ src/components/DataList.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import DataTable from "./DataTable";
+
+
+
+function DataList ({ filteredUsers }) {
+    console.log(filteredUsers)
+    return (
+        filteredUsers.map(person => (
+            <DataTable
+                key={person.login.uuid}
+                image={person.picture.medium}
+                name={person.name.first + " " + person.name.last}
+                phone={person.phone}
+                email={person.email}
+                dob={person.dob.date}
+                />
+
+        ))                                   
+    )
+}
+
+export default DataList; 
---
+++ src/components/DataTable.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Table from 'react-bootstrap/Table';
+import "../styles/DataTable.css";
+
+
+const DataTable = ({ login, image, name, phone, email, dob }) => {
+    return (
+        <Table >
+            <tbody>
+
+                <tr key={login}>
+
+                    <td data-th="Image">
+                        <img
+                            src={image}
+                            alt="person"
+                            className="image"
+                        />
+
+                    </td>
+                    <td data-th="Name">
+                        {name}
+
+                    </td>
+                    <td data-th="Phone Number">
+                        {phone}
+                    </td>
+                    <td data-th="Email">
+                        {email}
+                    </td>
+                    <td data-th="DOB">
+                        {dob}
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
+
+
+
+    )
+}
+
+
+export default DataTable; 
